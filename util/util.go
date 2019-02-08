c1cd6a268bfcf30217acbb1e8e0902cfa6649b5b
Rick Arnold <ntwrick@gmail.com>
2019-02-08 14:00:34 -0500

    Implement issue #164: abs history refactored and cd() util.ExpandPath()

--- util/util.go
+++ util/util.go
@@ -1,6 +1,10 @@
 package util
 
-import "strconv"
+import (
+	"os/user"
+	"path/filepath"
+	"strconv"
+)
 
 // Checks whether the element e is in the
 // list of strings s
@@ -17,4 +21,17 @@ func IsNumber(s string) bool {
 	_, err := strconv.ParseFloat(s, 64)
 
 	return err == nil
+}
+
+// ExpandPath (path) resolves leading "~/" to user's HomeDir
+// returns expanded path, error
+func ExpandPath(path string) (string, error) {
+	if len(path) == 0 || path[0] != '~' {
+		return path, nil
+	}
+	usr, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(usr.HomeDir, path[1:]), nil
 }
