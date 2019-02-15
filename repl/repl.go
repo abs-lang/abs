39772cb65a793d4d75001fc1385feb2a48b5222b
Rick Arnold <ntwrick@gmail.com>
2019-02-15 12:57:36 -0500

    refactor ABS Init File and live prompt support into init.go

--- repl/repl.go
+++ repl/repl.go
@@ -63,28 +63,6 @@ func changeLivePrefix() (string, bool) {
 	return livePrefix, LivePrefixState.IsEnable
 }
 
-// support for user config of ABS REPL prompt string
-const ABS_PROMPT_PREFIX = "⧐  "
-
-// format ABS_PROMPT_PREFIX = "{user}@{host}:{dir} $"
-func formatLivePrefix(prefix string) string {
-	livePrefix := prefix
-	if strings.Contains(prefix, "{") {
-		userInfo, _ := user.Current()
-		user := userInfo.Username
-		host, _ := os.Hostname()
-		dir, _ := os.Getwd()
-		// shorten homedir to ~/
-		homeDir := userInfo.HomeDir
-		dir = strings.Replace(dir, homeDir, "~", 1)
-		// format the livePrefix
-		livePrefix = strings.Replace(livePrefix, "{user}", user, 1)
-		livePrefix = strings.Replace(livePrefix, "{host}", host, 1)
-		livePrefix = strings.Replace(livePrefix, "{dir}", dir, 1)
-	}
-	return livePrefix
-}
-
 func Start(in io.Reader, out io.Writer) {
 	// get history file only when interactive REPL is running
 	historyFile, maxLines = getHistoryConfiguration()
@@ -198,31 +176,6 @@ func printParserErrors(errors []string) {
 	for _, msg := range errors {
 		fmt.Printf("%s", "\t"+msg+"\n")
 	}
-}
-
-// support for ABS init file
-const ABS_INIT_FILE = "~/.absrc"
-
-func getAbsInitFile(interactive bool) {
-	// get ABS_INIT_FILE from OS environment or default
-	initFile := os.Getenv("ABS_INIT_FILE")
-	if len(initFile) == 0 {
-		initFile = ABS_INIT_FILE
-	}
-	// expand the ABS_INIT_FILE to the user's HomeDir
-	filePath, err := util.ExpandPath(initFile)
-	if err != nil {
-		fmt.Printf("Unable to expand ABS init file path: %s\nError: %s\n", initFile, err.Error())
-		os.Exit(99)
-	}
-	initFile = filePath
-	// read and eval the abs init file
-	code, err := ioutil.ReadFile(initFile)
-	if err != nil {
-		// abs init file is optional -- nothing to do here
-		return
-	}
-	Run(string(code), interactive)
 }
 
 // BeginRepl (args) -- the REPL, both interactive and script modes begin here
