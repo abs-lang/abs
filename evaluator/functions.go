35447c885004e0342ae8d362ae0ba566f977f795
Adrian Ho <the.gromgit@gmail.com>
2020-08-06 17:38:11 +0800

    Make fmt() function more like printf()
    At least reduce the number of times Go's dreaded `%!d(string=hi)` appears on type mismatch

--- evaluator/functions.go
+++ evaluator/functions.go
@@ -1135,7 +1135,7 @@ func fmtFn(tok token.Token, env *object.Environment, args ...object.Object) obje
 	list := []interface{}{}
 
 	for _, s := range args[1:] {
-		list = append(list, s.Inspect())
+		list = append(list, s.Raw())
 	}
 
 	return &object.String{Token: tok, Value: fmt.Sprintf(args[0].(*object.String).Value, list...)}
