ab029140e45336f7bc1f29bf93dc2be0ca40c618
odino <alessandro.nadalin@gmail.com>
2021-04-04 10:49:49 +0400

    updated docs

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -22,6 +22,7 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com' }],
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;500&display=swap' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@300;400;500;700;900&display=swap' }],
   ],
 
   /**
